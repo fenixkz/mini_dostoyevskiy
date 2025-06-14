19351eeecc439c1014456fd24cf3cecc057459de
fenixkz <mazhitovayan@gmail.com>
2025-06-14 18:04:00 +0900

    Hyper param change

--- main.py
+++ main.py
@@ -152,11 +152,11 @@ def main():
     #### Hyperparameters
     device = f'cuda:{local_rank}' if torch.cuda.is_available() else 'cpu'
     context_length = 256
-    batch_size = 128
+    batch_size = 64
     vocab_size = tokenizer.get_vocab_size()
-    n_embedding = 128
-    n_heads = 4
-    n_layers = 8
+    n_embedding = 256
+    n_heads = 8
+    n_layers = 16
     dropout = 0.1
     max_epoch = 20000
     eval_interval = 1
